18de8fb26e38389045b485dcb4e5374963d1ef94
Chris Couzens <ccouzens@gmail.com>
2013-03-21 13:05:52 +0000

    Write an initial test script

---
+++ test/test_from_hash_to_string.rb
@@ -0,0 +1,37 @@
+require 'test/unit'
+require_relative '../lib/SQLAnywhereConnectionString'
+
+class TestFromHashToString < Test::Unit::TestCase
+	def test_empty_hash
+    cs = SQLAnywhereConnectionString.new
+    cs.add {}
+    assert_equal("", cs.to_s)
+	end
+
+  def test_multiple_parameters
+    cs = SQLAnywhereConnectionString.new
+    cs.add({
+      Server: 'demo12',
+      DBN: 'demo',
+      UID: 'dba',
+      PWD: 'sql',
+      })
+    assert_equal("Server=demo12;DBN=demo;UID=DBA;PWD=sql", cs.to_s)
+  end
+
+  def test_backslashes
+    cs = SQLAnywhereConnectionString.new
+    cs.add({
+      UID: 'back\slash',
+      })
+    assert_equal('UID=back\slash', cs.to_s)
+  end
+
+  def test_semicolons
+    cs = SQLAnywhereConnectionString.new
+    cs.add({
+      UID: 'semi;colon',
+      })
+    assert_equal('UID=semi;colon', cs.to_s)
+  end
+end
